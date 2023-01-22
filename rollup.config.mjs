f2770c4a8e49750f75c2ae4a759d9ef9c9b8be44
QVGK <45526481+QVGK@users.noreply.github.com>
2023-01-22 15:03:36 +0800

    V1.3.0

---
+++ rollup.config.mjs
@@ -0,0 +1,42 @@
+// Rollup Plugins
+import url from '@rollup/plugin-url'
+import svgr from '@svgr/rollup'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+
+import postcss from 'rollup-plugin-postcss'
+import typescript from '@rollup/plugin-typescript'
+
+// Package File
+import pkg from './package.json' assert {type: 'json'}
+
+export default {
+    input: 'src/index.tsx',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            exports: 'named',
+            sourcemap: true
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            exports: 'named',
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        url(),
+        svgr(),
+        resolve(),
+        commonjs(),
+
+        postcss({
+            modules: true
+        }),
+        typescript({
+            tsconfig: './tsconfig.json'
+        })
+    ]
+}
